ad2c2dbbca86e933b4bc2ba351c591804985b4b0
Lukasz <lukasz@bryohm.com>
2023-12-08 20:28:40 +0100

    convert hard coded triangle into obj

--- server.mjs
+++ server.mjs
@@ -2,24 +2,32 @@ import http from 'http';
 import fs from 'fs';
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/teapot.obj") {
-    fs.readFile("teapot.obj", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
+  if (req.url === '/teapot.obj') {
+    fs.readFile('teapot.obj', (err, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(data);
     });
     return;
   }
 
-  if (req.url === "/teapot.mjs") {
-    fs.readFile("teapot.mjs", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/javascript" });
+  if (req.url === '/triangle.obj') {
+    fs.readFile('triangle.obj', (err, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(data);
     });
     return;
   }
 
-  fs.readFile("index.html", (err, data) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  if (req.url === '/teapot.mjs') {
+    fs.readFile('teapot.mjs', (err, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
+      res.end(data);
+    });
+    return;
+  }
+
+  fs.readFile('index.html', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data);
   });
 });
